d92c5d94d92672a776eb24e5e2aab12d968c9ccc
lgthvinh <everwellmax@gmail.com>
2024-10-01 17:58:54 +0700

    replace

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector(".navbar").style.backgroundColor = "#000000";
+  } else {
+    document.querySelector(".navbar").style.backgroundColor = "transparent";
+  }
+}
+
+if (localStorage.getItem('lang') === null) {
+  localStorage.setItem('lang', navigator.language || navigator.userLanguage);
+}
+
+var userLang = localStorage.getItem('lang') || navigator.language || navigator.userLanguage;
+
+document.getElementById('language-selector').onclick = function switchWebLanguege() {
+  if (userLang === 'en') {
+    localStorage.setItem('lang', 'vi');
+  } else {
+    localStorage.setItem('lang', 'en');
+  }
+  console.log('Language changed to ' + localStorage.getItem('lang'));
+  window.location.href = '/';
+}
+
+document.getElementById('language-selector-main').onclick = function switchWebLanguege() {
+  if (userLang === 'en') {
+    localStorage.setItem('lang', 'vi');
+  } else {
+    localStorage.setItem('lang', 'en');
+  }
+  console.log('Language changed to ' + localStorage.getItem('lang'));
+  window.location.href = '/';
+}
+
+if (userLang !== 'vi' && !window.location.href.includes('en')) {
+  window.location.href = 'en';
+}
