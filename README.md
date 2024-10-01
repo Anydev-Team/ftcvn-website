41d1d8a06fab53534c1d3848cdd635d353a0878c
lgthvinh <everwellmax@gmail.com>
2024-10-01 17:51:24 +0700

    update: language switcher

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ This is the repository for the FIRST Tech Challenge Vietnam website for the 2024
 | Date       | Version | Description                |
 |------------|---------|----------------------------|
 | 2024-09-19 | 1.0.0   | Initial release            |
-| 2024-09-29 | 1.0.1   | Added English translation  |
+| 2024-09-29 | 1.1.0   | Added English translation  |
+| 2024-10-01 | 1.1.1   | Language selector          |
--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,32 @@
 window.onscroll = function() {
-  scrollFunction();
-}
-
-function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.querySelector(".navbar").style.backgroundColor = "#000000";
   } else {
     document.querySelector(".navbar").style.backgroundColor = "transparent";
   }
+}
+
+var userLang = localStorage.getItem('lang') || navigator.language || navigator.userLanguage;
+
+document.getElementById('language-selector').onclick = function switchWebLanguege() {
+  if (userLang === 'en') {
+    localStorage.setItem('lang', 'vi');
+  } else {
+    localStorage.setItem('lang', 'en');
+  }
+  console.log('Language changed to ' + localStorage.getItem('lang'));
+  window.location.href = '/';
+}
+document.getElementById('language-selector-main').onclick = function switchWebLanguege() {
+  if (userLang === 'en') {
+    localStorage.setItem('lang', 'vi');
+  } else {
+    localStorage.setItem('lang', 'en');
+  }
+  console.log('Language changed to ' + localStorage.getItem('lang'));
+  window.location.href = '/';
+}
+
+if (userLang !== 'vi' && !window.location.href.includes('en')) {
+  window.location.href = 'en';
 }
