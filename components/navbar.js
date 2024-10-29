a9f3099d5147df1a6717ad8eee0b3d5156e4e414
lgthvinh <everwellmax@gmail.com>
2024-10-29 10:04:34 +0700

    fix lang switcher on mobile view

--- components/navbar.js
+++ components/navbar.js
@@ -41,24 +41,25 @@ class Navbar extends LitElement {
             <a class="navbar-brand" href="#">
               <img src="/assets/images/ftcvn-logo.png" alt="" width="150" height="50" class="d-inline-block align-text-top">
             </a>
+
             <button class="navbar-toggler border-0" type="button">
-              <style>
-                #language-selector .dropdown-toggle::after {
-                  content: none;
-                }
-              </style>
-              <div class="nav-item d-inline-block px-2" style="position: initial;">
-                <span class="fi fi-vn">
-                  <a class="nav-link text-decoration-none border-0 shadow-none" name="lang-switcher" id="language-selector"></a>
-                </span>
-              </div>
-              <span class="bi bi-list fs-1" style="color: #ff8100;" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"></span>
-            </button>
+            <style>
+              #language-selector .dropdown-toggle::after {
+                content: none;
+              }
+            </style>
+            <div class="nav-item d-inline-block px-2" style="position: initial;">
+              <a class="nav-link text-decoration-none border-0 shadow-none" id="language-selector">
+                <span class="fi fi-vn"></span>
+              </a>
+            </div>
+            <span class="bi bi-list fs-1" style="color: #ff8100;" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"></span>
+          </button>
             <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item d-none d-lg-block">
                 <span class="fi fi-vn">
-                  <button class="nav-link text-decoration-none border-0 shadow-none" id="language-selector-main" name="lang-switcher"></button>
+                  <button class="nav-link text-decoration-none border-0 shadow-none" name="lang-switcher"></button>
                 </span>
                 </li>
                 <li class="nav-item">
@@ -161,9 +162,9 @@ class Navbar extends LitElement {
               }
             </style>
             <div class="nav-item d-inline-block px-2" style="position: initial;">
-                <span class="fi fi-us">
-                  <a class="nav-link text-decoration-none border-0 shadow-none" id="language-selector" name="lang-switcher"></a>
-                </span>
+              <a class="nav-link text-decoration-none border-0 shadow-none" id="language-selector">
+                <span class="fi fi-us"></span>
+              </a>
             </div>
             <span class="bi bi-list fs-1" style="color: #ff8100;" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"></span>
           </button>
@@ -171,17 +172,17 @@ class Navbar extends LitElement {
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item d-none d-lg-block">
                 <span class="fi fi-us">
-                  <button class="nav-link text-decoration-none border-0 shadow-none" id="language-selector-main" name="lang-switcher"></button>
+                  <button class="nav-link text-decoration-none border-0 shadow-none" name="lang-switcher"></button>
                 </span>
               </li>
               <li class="nav-item">
                 <a class="nav-link border-0 shadow-none text-white" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link border-0 shadow-none text-white" aria-current="page" href="/2324/aboutFirst.html">About <span class="fw-bold fst-italic">FIRST®</span></a>
+                <a class="nav-link border-0 shadow-none text-white" aria-current="page" href="/2324/en/aboutFirst">About <span class="fw-bold fst-italic">FIRST®</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link border-0 shadow-none text-white" aria-current="page" href="/2324/vorc-cantho.html">VORC 2024</a>
+                <a class="nav-link border-0 shadow-none text-white" aria-current="page" href="/2324/en/vorc-cantho">VORC 2024</a>
               </li>
               <li class="nav-item"></li>
               <a class="nav-link border-0 shadow-none text-white" aria-current="page" href="/faqs/">FAQs</span></a>
--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,38 @@
 const excludedLocalePaths = ['faqs', '2324'];
 
+var userLang = localStorage.getItem('lang') || navigator.language || navigator.userLanguage;
+
 function isExceptedPath() {
   return excludedLocalePaths.some(path => window.location.href.includes(path));
 }
 
-window.onscroll = function() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.querySelector(".navbar").style.backgroundColor = "#000000";
+function switchWebLanguege() {
+  console.log('Current language: ' + userLang);
+  if (userLang === 'en') {
+    localStorage.setItem('lang', 'vi');
   } else {
-    document.querySelector(".navbar").style.backgroundColor = "transparent";
+    localStorage.setItem('lang', 'en');
   }
+  console.log('Language changed to ' + localStorage.getItem('lang'));
+  window.location.href = '/'
 }
 
-if (localStorage.getItem('lang') === null) {
-  localStorage.setItem('lang', navigator.language || navigator.userLanguage);
-}
+document.addEventListener('DOMContentLoaded', function() {
+  window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.querySelector(".navbar").style.backgroundColor = "#000000";
+    } else {
+      document.querySelector(".navbar").style.backgroundColor = "transparent";
+    }
+  }
 
-var userLang = localStorage.getItem('lang') || navigator.language || navigator.userLanguage;
-// // function switchWebLanguege() {
-// //   console.log('Current language: ' + userLang);
-// //   if (userLang === 'en') {
-// //     localStorage.setItem('lang', 'vi');
-// //   } else {
-// //     localStorage.setItem('lang', 'en');
-// //   }
-// //   console.log('Language changed to ' + localStorage.getItem('lang'));
-// //   window.location.href = '/ftcvn-website';
-// // }
+  if (localStorage.getItem('lang') === null) {
+    localStorage.setItem('lang', navigator.language || navigator.userLanguage);
+  }
 
-// document.getElementById('language-selector').onclick = switchWebLanguege;
+  document.getElementById('language-selector').onclick = switchWebLanguege;
 
-// document.getElementById('language-selector-main').onclick = switchWebLanguege;
-
-if (userLang !== 'vi' && !window.location.href.includes('en') && !isExceptedPath()) {
-  window.location.href = 'en';
-}
+  if (userLang !== 'vi' && !window.location.href.includes('en') && !isExceptedPath()) {
+    window.location.href = 'en';
+  }
+});
